43e0b3fde754417911f5d4e869e8686bd2a793f7
zafchiel <bialakropla@proton.me>
2024-09-19 22:23:14 +0200

    upload and save images

--- main.go
+++ main.go
@@ -2,21 +2,29 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 
 	"github.com/zafchiel/image-service/middleware"
+)
+
+const (
+	port          = ":8080"
+	maxUploadSize = 10 << 20 // 10 MB
 )
 
 func main() {
 	router := http.NewServeMux()
 	router.HandleFunc("/", hello)
+	router.HandleFunc("POST /upload", uploadImage)
 
 	server := http.Server{
-		Addr:    ":8080",
+		Addr:    port,
 		Handler: middleware.Logger(router),
 	}
 
-	fmt.Println("Server is running on port 8080")
+	fmt.Println("Server is running on port", port)
 	if err := server.ListenAndServe(); err != nil {
 		fmt.Println("Error starting server:", err)
 	}
@@ -24,4 +32,45 @@ func main() {
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello, World!")
+}
+
+func uploadImage(w http.ResponseWriter, r *http.Request) {
+	// Parse the multipart form data
+	if err := r.ParseMultipartForm(maxUploadSize); err != nil {
+		http.Error(w, "The uploaded file is too big. Please upload a file up to 10MB", http.StatusBadRequest)
+		return
+	}
+
+	file, header, err := r.FormFile("image")
+	if err != nil {
+		http.Error(w, "No image file uploaded", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	contentType := header.Header.Get("Content-Type")
+	if contentType != "image/jpeg" && contentType != "image/png" {
+		http.Error(w, "Only jpeg and png images are allowed", http.StatusBadRequest)
+		return
+	}
+
+	if header.Size > maxUploadSize {
+		http.Error(w, fmt.Sprintf("The uploaded image is too big: %v. Please upload an image up to %v", header.Size, maxUploadSize), http.StatusBadRequest)
+		return
+	}
+
+	destination, err := os.Create("images/" + header.Filename)
+	if err != nil {
+		http.Error(w, "Failed to create file on server", http.StatusInternalServerError)
+		return
+	}
+	defer destination.Close()
+
+	_, err = io.Copy(destination, file)
+	if err != nil {
+		http.Error(w, "Failed to save file on server", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "File %s uploaded successfully. Size: %d bytes, Type: %s\n", header.Filename, header.Size, contentType)
 }
