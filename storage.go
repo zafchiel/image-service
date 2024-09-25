cb5cd1adb17680788831177fe689eb3758fc0256
zafchiel <bialakropla@proton.me>
2024-09-25 15:23:05 +0200

    feat: local storage inteface implementation

--- storage.go
+++ storage.go
@@ -1,6 +1,10 @@
 package main
 
-import "io"
+import (
+	"io"
+	"os"
+	"path/filepath"
+)
 
 type Storage interface {
 	Save(filename string, file io.Reader) error
@@ -11,4 +15,26 @@ type Storage interface {
 type LocalStorage struct {
 	// Base path to store the files
 	root string
+}
+
+func (ls *LocalStorage) Save(filename string, content io.Reader) error {
+	fullPath := filepath.Join(ls.root, filename)
+	file, err := os.Create(fullPath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, content)
+	return err
+}
+
+func (ls *LocalStorage) Get(filename string) (io.Reader, error) {
+	fullPath := filepath.Join(ls.root, filename)
+	return os.Open(fullPath)
+}
+
+func (ls *LocalStorage) Delete(filename string) error {
+	fullPath := filepath.Join(ls.root, filename)
+	return os.Remove(fullPath)
 }
